dc5152832eeba2712bd87209b8592de3e2ef257c
mbhall88 <mbhall88@gmail.com>
2017-02-26 15:23:17 +1000

    test npReader and npAnalysis. Cant get them to work.

---
+++ public/data/japsaTesting/speciesTypingMac.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#Usage: speciesTyping.sh folder_of_f5 path_to_virusDB
+
+folder=$1
+sdb=$2
+
+jsa.np.npreader -realtime -GUI --fail --folder $folder --output - 2> npreader.log | bwa mem -t 4 -k11 -W20 -r10 -A1 -B1 -O1 -E1 -L0 -Y -K 10000 $sdb/genomeDB.fasta $fq 2> /dev/null | jsa.np.rtSpeciesTyping -bam - -index $sdb/speciesIndex --read 100 -time 0 -out species.dat 2> species.log
