a7aaf8866e2d16f3272e4d2d9659255c2f6c393c
Michael Hall <mbhall88@gmail.com>
2017-02-27 18:08:49 +1000

    create .gitignore and add rules

--- routes/analysis.js
+++ routes/analysis.js
@@ -8,8 +8,12 @@ var express  = require('express'),
     router   = express.Router();
 
 const exec = require('child_process').exec;
+const spawn = require('child_process').spawn;
 
-const SCRIPT_DIR = path.join(__dirname, '../public/data/');
+const SCRIPT_DIR = path.join(__dirname, '../public/data/japsaTesting/');
+const VIRUS_DB = path.join(__dirname, '../public/data/virusDB/');
+// command to run species typing
+const RUN_ST = SCRIPT_DIR + "speciesTypingMac.sh " + SCRIPT_DIR + "testZika/ " + VIRUS_DB;
 
 
 
@@ -19,23 +23,69 @@ router.get('/', function(req, res, next) {
 });
 
 // POST route
-router.post('/', function(req, res, next){
-    console.log("Post request received...");
-    exec(SCRIPT_DIR + 'test.sh', function(err, stdout, stderr){
-        if (err){
-            console.log(err);
-        } else {
-            console.log(stdout);
-            console.log(stderr);
-            chokidar.watch(SCRIPT_DIR, {ignored: /(^|[\/\\])\../}).on('all', function(event, path){
-                console.log(event, path);
-            });
-        }
-    });
-    // this runs before the stdout gets returned. If I want it to run last then I should use
-    // execSync()
-    console.log("I should be last");
+// router.post('/', function(req, res, next){
+//     console.log("Post request received...");
+//     spawn(RUN_ST, function(err, stdout, stderr){
+//         if (err){
+//             console.log("Err: " + err);
+//         } else {
+//         	console.log("No errors...");
+//             console.log("STDOUT: " + stdout);
+//             console.log("STDERR: " + stderr);
+//             chokidar.watch(SCRIPT_DIR, {ignored: /(^|[\/\\])\../}).on('all', function(event, path){
+//                 console.log(event, path);
+//             });
+//         }
+//     });
+// });
 
+router.post('/', function(req, res){
+	console.log("Post request received...");
+
+	// arguments (in order) to be passed to the child process
+	var scriptArgs = ['speciesTypingMac.sh', 'testZika/', '../virusDB/'];
+
+	var scriptOptions = {
+		// this is the directory which the child process will be run on
+		cwd: SCRIPT_DIR
+	};
+
+	// creating the child process
+	const speciesTyping = spawn('sh', scriptArgs, scriptOptions);
+
+	speciesTyping.stdout.setEncoding('utf8');
+
+	// handle error in trying to run spawn command
+	speciesTyping.on('error', function(err){
+		console.log("Spawn has error: " + err);
+	});
+
+	// handle STDERR coming from child process
+	speciesTyping.stderr.on('data', function(chunk){
+		// console.log("STDERR: " + chunk);
+	});
+
+	// handle STDOUT coming from child process. This should be the species typing output
+	speciesTyping.stdout.on('data', function(chunk){
+		console.log("STDOUT: " + chunk);
+		parseSpecTypingResults(chunk);
+	});
+
+	speciesTyping.on('close', function(code){
+		if (code !== 0){
+			console.log("Process exited with code " + code);
+		}
+	});
 });
+
+
+// function to parse species typing output
+function parseSpecTypingResults(stdout){
+	if (stdout.startsWith("time")){
+		console.log("Header detected");
+
+	}
+}
+
 
 module.exports = router;
