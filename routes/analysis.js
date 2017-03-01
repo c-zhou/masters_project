a3e15b1ff07bff2ab46cdbe5f1250c9c75f34af4
Michael Hall <mbhall88@gmail.com>
2017-03-01 17:56:34 +1000

    add donut chart to display species typing output

--- routes/analysis.js
+++ routes/analysis.js
@@ -16,7 +16,7 @@ const VIRUS_DB = path.join(__dirname, '../public/data/virusDB/');
 const RUN_ST = SCRIPT_DIR + "speciesTypingMac.sh " + SCRIPT_DIR + "testZika/ " + VIRUS_DB;
 
 // require middleware with donut chart function
-var chart = require('../middleware/donut.js');
+// var chart = require('../middleware/donut.js');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -56,12 +56,10 @@ router.post('/', function(req, res){
 
 	// handle STDOUT coming from child process. This should be the species typing output
 	speciesTyping.stdout.on('data', function(chunk){
-		chart([50, 25, 25], '/Users/m.hall/Documents/test.svg');
 		console.log("STDOUT: " + chunk);
 		var info = parseSpecTypingResults(chunk);
 		var prob = +info.prob * 100;
-		socket.emit('stdout', "Species: " + info.species +
-			"\nProbability: " + Math.round(prob * 100) / 100 + "%");
+		socket.emit('stdout', info);
 		// res.render("analysis", {info: parseSpecTypingResults(chunk)});
 	});
 
@@ -102,7 +100,18 @@ function st2JSON(line){
 
 	// add headings as keys and associate their values from the parsed line
 	for (var i = 0; i < headings.length; i++){
-		output[headings[i]] = results[i];
+		if (!isNaN(results[i])){
+			output[headings[i]] = +results[i];
+			if (headings[i] === "prob"){
+				// TODO - bug with some numbers not rounding properly i.e 99.770000000000001
+				output[headings[i]] = Math.round(output[headings[i]] * 10000) / 10000;
+			}
+		} else {
+			if (headings[i] === "species"){
+				results[i] = results[i].split(" ").slice(0, 4).join(" ");
+			}
+			output[headings[i]] = results[i];
+		}
 	}
 	return output;
 }
