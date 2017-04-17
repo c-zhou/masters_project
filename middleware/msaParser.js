89da8ba7225a2b4066ef488c5f8248a53d33ac90
mbhall88 <mbhall88@gmail.com>
2017-04-17 12:20:37 +1000

    add multiple sequence alignment parser. testing

---
+++ middleware/msaParser.js
@@ -0,0 +1,52 @@
+/**
+ * Created by m.hall on 22/3/17.
+ */
+
+const fs = require('fs'),
+	  readline = require('readline');
+
+var d3 = require('d3');
+
+
+
+
+// function that returns an object with keys as the headers of the tsv file and values
+// for those headers in an array
+var parser = function(fileName, callback) {
+
+	// read from input readable stream, but one line at a time
+	const rl = readline.createInterface({
+		      input: fs.createReadStream(fileName)
+	      });
+
+	var data = {
+            sampleID: [],
+            sequence: [],
+            gene_start: null,
+            gene_end: null
+		},
+	    lines = 0; // track line numbers for determining head etc.
+
+	// reading file one line at a time
+	rl.on('line', function(line) {
+		var row = line.split(' ');
+		console.log(row);
+		// if (lines === 0) { // headers/keys
+		// 	keys = row;
+		// 	keys.forEach(function(currentValue) {
+		// 		data[currentValue] = []; // create key in object for each header
+		// 	});
+		// } else {
+		// 	row.forEach(function(currentValue, i) {
+		// 		data[keys[i]].push(currentValue); // push values into appropriate key array
+		// 	});
+		// }
+		// lines++;
+	});
+
+	rl.on('close', function() {
+		return callback(data); // when done, run the given callback function on the data
+	});
+}
+
+module.exports = parser;
--- routes/dashboard.js
+++ routes/dashboard.js
@@ -3,12 +3,12 @@
  */
 var express    = require('express'),
     router     = express.Router(),
-	sankeyParser = require('../middleware/sankeyParser');
+	msaParser = require('../middleware/msaParser');
 
-var sankeyFile = 'public/data/sankey.tsv';
+var testFile = 'public/data/sankey.tsv';
 
 router.get('/', function(req, res) {
-	sankeyParser.tsvParser(sankeyFile, function(data) {
+	msaParser.parser(testFile, function(data) {
 		var diagramData = sankeyParser.graphParser(data);
 		// one parsing is complete, render response
 		res.render('dashboard', { fixtureData: JSON.stringify(diagramData) });
