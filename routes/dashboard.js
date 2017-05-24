ec61d3ac3d37d9064f39731db5e672e81959bcbb
Michael Hall <mbhall88@gmail.com>
2017-05-24 15:11:46 +1000

    try load mgrb file

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -5,7 +5,9 @@ var express    = require('express'),
     router     = express.Router(),
 	msaParser = require('../middleware/msaParser');
 
-var testFile = 'public/data/greece_toy_data.txt';
+// var testFile = 'public/data/greece_toy_data.txt';
+var testFile = 'public/data/mgrb_aln.dat';
+// var testFile = 'public/data/phoq_aln.dat';
 
 router.get('/', function(req, res) {
 	msaParser(testFile, function(data) {
