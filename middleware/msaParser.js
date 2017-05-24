2de0fe410ba83669d2195e56026c6f0bc989244e
Michael Hall <mbhall88@gmail.com>
2017-05-24 11:15:37 +1000

    logging the dashboard data structure for methods writing

--- middleware/msaParser.js
+++ middleware/msaParser.js
@@ -48,7 +48,7 @@ var parser = function(fileName, callback) {
 
 		var tData = transform(data);
 
-		console.log(tData.data);
+		console.log(tData.data[7].values);
 
 		return callback(tData); // when done, run the given callback function on the data
 	});
