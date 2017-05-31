85245b5fa4d2bf1d94dfdb7803119dc9aae36ab1
Michael Hall <mbhall88@gmail.com>
2017-05-31 12:23:43 +1000

    change position of tooltip

--- public/javascripts/dashboard.js
+++ public/javascripts/dashboard.js
@@ -279,7 +279,7 @@ function parallelCoordinates() {
 	        }
 
 	        function tipMove() {
-		        tooltip.style("top", (event.pageY) + "px")
+		        tooltip.style("top", (event.pageY + 5) + "px")
 			        .style("left", (event.pageX + 20) + "px");
 	        }
 
